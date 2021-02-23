d86bb6b0ff0b70f425ab9cb5f2e0a2b1e34601a5
unknown <manuchaturvedi28mc2gmail.com>
2021-02-23 23:13:48 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,25 @@
+const toggleButton = document.getElementsByClassName('toggle-button')[0]
+const navbarLinks = document.getElementsByClassName('navbar-links')[0]
+
+toggleButton.addEventListener('click', () => {
+  navbarLinks.classList.toggle('active')
+  })
+
+  $('.carousel').carousel({
+    interval: 10000
+  })
+
+  function initMap() {
+    // The location of Uluru
+    const uluru = { lat: -25.344, lng: 131.036 };
+    // The map, centered at Uluru
+    const map = new google.maps.Map(document.getElementById("map"), {
+      zoom: 4,
+      center: uluru,
+    });
+    // The marker, positioned at Uluru
+    const marker = new google.maps.Marker({
+      position: uluru,
+      map: map,
+    });
+  }
